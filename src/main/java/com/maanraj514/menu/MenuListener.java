4b68e1f9cd352a7ef178a7ce12a699fbb692a225
BuildTools <unconfigured@null.spigotmc.org>
2023-08-04 18:21:05 +0800

    hopefully this fixes an issue

--- src/main/java/com/maanraj514/menu/MenuListener.java
+++ src/main/java/com/maanraj514/menu/MenuListener.java
@@ -3,6 +3,7 @@ package com.maanraj514.menu;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
 /**
@@ -12,6 +13,10 @@ public class MenuListener implements Listener {
 
     @EventHandler
     public void onMenuClick(InventoryClickEvent event){
+        Inventory inventory = event.getClickedInventory();
+        if (inventory == null){
+            return;
+        }
         InventoryHolder holder = event.getClickedInventory().getHolder();
 
         if (holder instanceof Menu){
