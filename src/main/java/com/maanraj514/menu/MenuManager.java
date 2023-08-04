52517f178e4b22e2ca7cfda17b297985f6dcec7b
BuildTools <unconfigured@null.spigotmc.org>
2023-08-04 17:29:54 +0800

    hopefully this fixes an issue

--- src/main/java/com/maanraj514/menu/MenuManager.java
+++ src/main/java/com/maanraj514/menu/MenuManager.java
@@ -13,21 +13,25 @@ public class MenuManager {
     private static final HashMap<Player, PlayerMenuUtility> playerMenuUtilityMap = new HashMap<>();;
 
     public static void openMenu(Class<? extends Menu> menuClass, Player player) {
-        try{
+        try {
             menuClass.getConstructor(PlayerMenuUtility.class).newInstance(getPlayerMenuUtility(player)).open();
-        }  catch (InvocationTargetException | InstantiationException | NoSuchMethodException | IllegalAccessException e) {
-            throw new RuntimeException(e);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
         }
     }
 
-    public static PlayerMenuUtility getPlayerMenuUtility(Player player){
-        if (playerMenuUtilityMap.containsKey(player)){
-            return playerMenuUtilityMap.get(player);
-        }else{
-            PlayerMenuUtility playerMenuUtility = new PlayerMenuUtility(player);
-            playerMenuUtilityMap.put(player, playerMenuUtility);
+    public static PlayerMenuUtility getPlayerMenuUtility(Player p) {
+
+        PlayerMenuUtility playerMenuUtility;
+        if (!(playerMenuUtilityMap.containsKey(p))) { //See if the player has a pmu "saved" for them
+
+            //Construct PMU
+            playerMenuUtility = new PlayerMenuUtility(p);
+            playerMenuUtilityMap.put(p, playerMenuUtility);
 
             return playerMenuUtility;
+        } else {
+            return playerMenuUtilityMap.get(p); //Return the object by using the provided player
         }
     }
 }
